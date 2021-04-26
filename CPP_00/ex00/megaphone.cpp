3b9e264165b300f888013ec2513057d0f57b0543
Andrea Duregon <aduregon@e3r9p7.42roma.it>
2021-04-26 19:23:56 +0200

    finish

--- CPP_00/ex00/megaphone.cpp
+++ CPP_00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: aduregon <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/20 15:08:06 by aduregon          #+#    #+#             */
-/*   Updated: 2021/04/20 15:23:59 by aduregon         ###   ########.fr       */
+/*   Updated: 2021/04/26 19:23:15 by aduregon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int	main(int argc, char **argv)
 		exit(0);
 	}
 	for (int j = 1; j < argc; j++)
-		for(int i = 0; i < strlen(argv[j]); i++)
+		for(int i = 0; i < int(strlen(argv[j])); i++)
 		{
 			std::cout << char(toupper(argv[j][i]));
 		}
