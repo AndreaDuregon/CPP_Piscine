d8620e82724e61a5ba3c5c5dd3fb62054874305c
Andrea Duregon <andreaduregon93@gmail.com>
2021-04-27 10:25:39 +0200

    panther finish

---
+++ CPP_01/ex01/ex01.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ex01.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aduregon <aduregon@42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/27 10:16:08 by aduregon          #+#    #+#             */
+/*   Updated: 2021/04/27 10:23:46 by aduregon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+void	memoryLeak()
+{
+	std::string* panther = new std::string("String panther");
+	std::cout << *panther << std::endl;
+	delete panther;
+}
+
+int	main ()
+{
+	memoryLeak();
+}
