c061402050de30d4234775a60a4ca6aa9c54427c
Andrea Duregon <aduregon@e3r9p5.42roma.it>
2021-05-09 19:00:00 +0200

    08 starting

--- CPP_07/ex00/Awesome.hpp
+++ CPP_07/ex00/Awesome.hpp
@@ -6,7 +6,7 @@
 /*   By: aduregon <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/09 16:28:08 by aduregon          #+#    #+#             */
-/*   Updated: 2021/05/09 17:11:41 by aduregon         ###   ########.fr       */
+/*   Updated: 2021/05/09 17:46:19 by aduregon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 class Awesome
 {
 public:
-	Awesome() {}
+	//Awesome() {}
 	Awesome( int n ) : _n( n ) {}
 	bool operator==( Awesome const & rhs ) { return (this->_n == rhs._n); }
 	bool operator!=( Awesome const & rhs ) { return (this->_n != rhs._n); }
--- CPP_08/ex00/main.cpp
+++ CPP_08/ex00/main.cpp
@@ -1,43 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aduregon <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/05/08 20:37:28 by aduregon          #+#    #+#             */
-/*   Updated: 2021/05/09 17:46:06 by aduregon         ###   ########.fr       */
+/*   Created: 2021/05/09 17:29:00 by aduregon          #+#    #+#             */
+/*   Updated: 2021/05/09 17:29:01 by aduregon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#pragma once
-
-#include <iostream>
-
-template <typename T>
-void	swap(T &t1, T &t2)
-{
-	T temp = t1;
-
-	temp = t1;
-	t1 = t2;
-	t2 = temp;
-}
-
-template <typename T>
-T		min(T &t1, T &t2)
-{
-	if (t1 < t2)
-		return (t1);
-	else
-		return (t2);
-}
-
-template <typename T>
-T		max(T &t1, T &t2)
-{
-	if (t1 > t2)
-		return (t1);
-	else
-		return (t2);
-}
