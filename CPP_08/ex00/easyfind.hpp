a08e2b699f447592f677e8aa4ed09656dd19868f
Andrea Duregon <andreaduregon93@gmail.com>
2021-05-09 23:25:40 +0200

    ex00 to check

--- CPP_08/ex00/easyfind.hpp
+++ CPP_08/ex00/easyfind.hpp
@@ -3,15 +3,33 @@
 /*                                                        :::      ::::::::   */
 /*   easyfind.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aduregon <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: aduregon <aduregon@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/09 17:29:10 by aduregon          #+#    #+#             */
-/*   Updated: 2021/05/09 18:01:25 by aduregon         ###   ########.fr       */
+/*   Updated: 2021/05/09 23:24:56 by aduregon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 #include <iostream>
+#include <algorithm>
+
+class NotFindException : public std::exception
+{
+public:
+	NotFindException() throw() {}
+	const char *what() const throw()
+	{
+		return ("Value not find!");
+	}
+};
 
 template<typename T>
+int		easyfind(T cont, int n)
+{
+	typename T::iterator it = std::find(cont.begin(), cont.end(), n);
+	if (it == cont.end())
+		throw NotFindException();
+	return (*it);
+}
--- CPP_08/ex00/main.cpp
+++ CPP_08/ex00/main.cpp
@@ -3,10 +3,35 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aduregon <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: aduregon <aduregon@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/09 17:29:00 by aduregon          #+#    #+#             */
-/*   Updated: 2021/05/09 17:29:01 by aduregon         ###   ########.fr       */
+/*   Updated: 2021/05/09 23:24:51 by aduregon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "easyfind.hpp"
+#include <vector>
+
+int main()
+{
+	std::vector<int>	vec;
+	
+	vec.push_back(1);
+	vec.push_back(10);
+	vec.push_back(100);
+	vec.push_back(1000);
+	vec.push_back(10000);
+
+	try
+	{
+		std::cout << easyfind(vec, 100) << std::endl;
+		std::cout << easyfind(vec, 1) << std::endl;
+		std::cout << easyfind(vec, 0) << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	
+}
