17a56f7ea34e30979c106791983a357142e387c3
Andrea Duregon <andreaduregon93@gmail.com>
2021-05-10 01:39:20 +0200

    ex02 ready to push

---
+++ CPP_08/ex02/MutantStack.hpp
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mutantstack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aduregon <aduregon@42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/10 00:53:10 by aduregon          #+#    #+#             */
+/*   Updated: 2021/05/10 01:30:29 by aduregon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <algorithm>
+#include <iostream>
+#include <stack>
+#include <list>
+
+template <typename T>
+class MutantStack : public std::stack<T>
+{
+private:
+	std::list<T>	stack;
+public:
+	MutantStack<T>(/* args */)	{}
+	MutantStack<T>(MutantStack const &copy)
+	{
+		this->stack = copy.stack;
+	}
+	MutantStack &operator = (MutantStack const &op)
+	{
+		this->stack = op.getStack();
+		return (*this);
+	}
+	~MutantStack<T>()	{}
+	std::list<T>	getStack() const
+	{
+		return (this->stack);
+	}
+	bool	empty()
+	{
+		return (this->stack.empty());
+	}
+	size_t	size()
+	{
+		return (this->stack.size());
+	}
+	T		&top()
+	{
+		return (this->stack.front());
+	}
+	void	push(T t)
+	{
+		return (this->stack.push_front(t));
+	}
+	void	pop()
+	{
+		return (this->stack.pop_front());
+	}
+
+	typedef typename std::list<T>::iterator		iterator;
+
+	iterator		begin()
+	{
+		return (this->stack.begin());
+	}
+	iterator		end()
+	{
+		return (this->stack.end());
+	}
+};
+
---
+++ CPP_08/ex02/main.cpp
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aduregon <aduregon@42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/10 00:53:26 by aduregon          #+#    #+#             */
+/*   Updated: 2021/05/10 01:38:40 by aduregon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "MutantStack.hpp"
+
+int main ()
+{
+	MutantStack<int> mstack;
+	std::cout << "is empty: " << mstack.empty() << std::endl;
+	std::cout << std::endl;
+	mstack.push(5);
+	mstack.push(17);
+	mstack.push(21);
+	mstack.push(33);
+	mstack.push(42);
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+	while (it != ite)
+	{
+	std::cout << *it << " ";
+	++it;
+	}
+	std::cout << std::endl;
+	std::cout << std::endl;
+	std::cout << "is empty: " << mstack.empty() << std::endl;
+	std::cout << std::endl;
+	std::cout << "size: " << mstack.size() << std::endl;
+	std::cout << std::endl;
+	std::cout << "top: " << mstack.top() << std::endl;
+	std::cout << std::endl;
+	mstack.pop();
+	std::cout << "top after pop: " << mstack.top() << std::endl;
+	std::cout << std::endl;
+	std::cout << "size after pop: " << mstack.size() << std::endl;
+	std::cout << std::endl;
+	mstack.pop();
+	mstack.pop();
+	mstack.pop();
+	it = mstack.begin();
+	ite = mstack.end();
+	while (it != ite)
+	{
+	std::cout << *it << " ";
+	++it;
+	}
+	std::cout << std::endl;
+	std::stack<int> s(mstack);
+	return 0;
+}
